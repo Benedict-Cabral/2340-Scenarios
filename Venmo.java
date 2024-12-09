d43d31fc595cce14156f503d13740def487da9b1
Benedict-Cabral <benedictemmanuel.cabral@gmail.com>
2024-12-09 14:45:10 -0500

    Delete Venmo.java

--- Venmo.java
+++
@@ -1,6 +0,0 @@
-public class Venmo implements PaymentMethod {
-    @Override
-    public void makePayment(double price) {
-        System.out.println("Payment of $" + price + " made with Venmo.");
-    }
-}
