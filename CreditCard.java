843d72aa0f82e9f270f1c275edb2f1fe391c0ea2
Benedict-Cabral <benedictemmanuel.cabral@gmail.com>
2024-12-09 14:44:42 -0500

    Delete CreditCard.java

--- CreditCard.java
+++
@@ -1,8 +0,0 @@
-public class CreditCard implements PaymentMethod {
-    @Override
-    public void makePayment(double price) {
-        System.out.println("Payment of $" + price + " made with a credit card.");
-    }
-}
-
-    
